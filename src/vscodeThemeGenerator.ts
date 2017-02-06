b24272ec7fd2b3832477782f1ae43450cca79046
Daniel Imms <daimms@microsoft.com>
2017-02-06 10:00:08 -0800

    Markdown bold/italic -> identifier color
    Part of Tyriar/vscode-theme-sapphire#6

--- src/vscodeThemeGenerator.ts
+++ src/vscodeThemeGenerator.ts
@@ -182,13 +182,12 @@ const vscodeJsonThemeRules: IRuleGenerator[] = [
    * Markdown
    */
   { source: set => set.syntax.keyword,       generate: getSimpleColorGenerator('Markdown heading', 'markup.heading') },
-  { source: set => set.syntax.identifier,    generate: getSimpleColorGenerator('Markdown link text', 'text.html.markdown meta.link.inline, meta.link.reference') },,
+  { source: set => set.syntax.identifier,    generate: getSimpleColorGenerator('Markdown link text', 'text.html.markdown meta.link.inline, meta.link.reference') },
   { source: set => set.syntax.markdownQuote, generate: getSimpleColorGenerator('Markdown block quote', 'text.html.markdown markup.quote') },
   { source: set => set.syntax.keyword,       generate: getSimpleColorGenerator('Markdown list item', 'text.html.markdown beginning.punctuation.definition.list') },
-  // TODO: Clean up getFontStyleGenerator
-  { source: set => 'undefined', generate: getFontStyleGenerator('Markdown italic', 'markup.italic', FontStyle.ITALIC) },
-  { source: set => 'undefined', generate: getFontStyleGenerator('Markdown bold', 'markup.bold', FontStyle.BOLD) },
-  { source: set => 'undefined', generate: getFontStyleGenerator('Markdown bold italic', 'markup.bold markup.italic, markup.italic markup.bold', FontStyle.BOLD | FontStyle.ITALIC) },
+  { source: set => set.syntax.identifier,    generate: getSimpleColorGenerator('Markdown italic', 'markup.italic', FontStyle.ITALIC) },
+  { source: set => set.syntax.identifier,    generate: getSimpleColorGenerator('Markdown bold', 'markup.bold', FontStyle.BOLD) },
+  { source: set => set.syntax.identifier,    generate: getSimpleColorGenerator('Markdown bold italic', 'markup.bold markup.italic, markup.italic markup.bold', FontStyle.BOLD | FontStyle.ITALIC) },
   /**
    * Ini
    */
