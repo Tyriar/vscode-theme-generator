999d5d9a0f9799bb3cab159174da1db929ff8b6a
Daniel Imms <tyriar@tyriar.com>
2017-05-05 19:48:22 -0700

    Refine ts example

--- language-examples/typescript.ts
+++ language-examples/typescript.ts
@@ -1,3 +1,19 @@
+// An exceptionally useful comment
+function func(param: string): object {
+    var text = 'string';
+    for (var i = 0; i < param.length; i++) {
+        text += i;
+    }
+    var number = 0;
+    var templateLiterals = `a ${text} b ${1 + 2} c`;
+    return {
+        "text": text,
+        "boolean": false,
+        "number": 231
+    };
+}
+func('abc');
+
 import { a } from 'some-file';
 
 /**
